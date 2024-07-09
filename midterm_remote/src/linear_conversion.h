f9ee35e7efdea95bfb4cecffccc7e8048ce30865
mattysc42 <mattysc42@gmail.com>
2024-07-09 16:56:29 -0600

    made fixes to the fritzing and started the code for the project

---
+++ midterm_remote/src/linear_conversion.h
@@ -0,0 +1,20 @@
+#ifndef _LINEAR_CONVERSION_
+#define _LINEAR_CONVERSION_
+
+float findSlope(float x1, float y1, float x2, float y2) {
+    float slope = (y2 - y1) / (x2 - x1);
+    return slope;
+}
+
+float findYInstercept(float slope, float x1, float y1) {
+    float yIntercept = y1 - (slope * x1);
+    return yIntercept;
+}
+
+// Third input value is the current value that you're trying to map. Example: an encoder's current value.
+float findLinearConversion(float slope, float yIntercept, float valueToMap) {
+    float yOut = (slope * valueToMap) + yIntercept;
+    return yOut;
+}
+
+#endif
